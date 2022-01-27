f515232d0fe25771d14a7e88b04f37936c0c23c6
Marveeen <jmvroqueee@gmail.com>
2022-01-27 15:03:52 +0800

    Home snippet added

--- js/script.js
+++ js/script.js
@@ -10,4 +10,38 @@ $(function() {
     $("#navbarToggle").on("click", function (e) {
         $(e.target).focus();
     })
-})
+});
+
+(function (global) {
+    let dc = {};
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    //convinience function for inserting innerHTML for 'select'
+    let insertHTML = (selector, html) => {
+        let targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    }
+
+    // Show loading icon inside element identified by 'selector'.
+    let showLoading = (selector) => {
+        let html = `<div class='text-center'>`;
+        html += `<img src='./images/ajax-loader.gif'></div>`
+        insertHTML(selector, html);
+    }
+
+    // On page load (before images or CSS)
+    document.addEventListener("DOMContentLoaded", function(event) {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            homeHtml,
+            function (responseText) {
+                document.querySelector("#main-content").innerHTML = responseText;
+            },
+            false
+        );
+    })
+
+    global.$dc = dc;
+    
+})(window);
